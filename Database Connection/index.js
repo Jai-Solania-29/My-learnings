e0ff19e8e7491d46edb84cbca98f68c3879e03c5
Jai-Solania-29 <jaideep2912@gmail.com>
2024-05-03 21:35:05 +0530

    JWT Assignment Completed

---
+++ Database Connection/index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://admin:fccLSOlPfCKcC1CR@cluster1.kn9szlj.mongodb.net/")
+
+const User = mongoose.model('Users', { name: String, email: String, password: String});
+
+app.post("/signup", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser = User.findOne({ email: username });
+
+    if (existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+
+    const user = new User({
+        name: name,
+        email: username,
+        password: password
+    });
+
+    user.save();
+    res.json({
+        msg: "User created successfully"
+    })
+})
+
+app.listen(3000);
