1d479b2030e8fb8ade68a19088ca2228ed6345c7
Jai-Solania-29 <jaideep2912@gmail.com>
2024-05-29 23:28:18 +0530

    Backend-Frontend Connection Completed

--- React Foundations/todo-app/backend/db.js
+++ React Foundations/todo-app/backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const { string, boolean } = require("zod");
 
-mongoose.connect("mongodb+srv://jaideep2912:brVkOEkPB664DkLr@cluster1.kdm9pbv.mongodb.net/")
+mongoose.connect("mongodb+srv://jaideep2912:brVkOEkPB664DkLr@cluster1.kdm9pbv.mongodb.net/todo")
 
 const todoSchema = mongoose.Schema({
-    title: string,
-    description: string,
-    completed: boolean
+    title: String,
+    description: String,
+    completed: Boolean
 })
 
 const todo = mongoose.model('todos', todoSchema);
